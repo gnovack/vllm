176da67b62ac41741d650beca6ee5b0d369ba25f
gnovack <gnovack@amazon.com>
2025-10-27 19:12:24 +0000

    improve gptoss lora tests
    Signed-off-by: gnovack <gnovack@amazon.com>

--- tests/lora/test_gptoss.py
+++ tests/lora/test_gptoss.py
@@ -1,23 +1,33 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
+import regex as re
+
 import vllm
 from vllm.lora.request import LoRARequest
 
 MODEL_PATH = "openai/gpt-oss-20b"
 
-# PROMPT_TEMPLATE = "<｜begin▁of▁sentence｜>You are a helpful assistant.\n\nUser: {context}\n\nAssistant:"  # noqa: E501
-PROMPT_TEMPLATE = "Question: {question}\nAnswer: "  # noqa: E501
+PROMPT_TEMPLATE = "Question: {question}\n\nQuery: "  # noqa: E501
 
 
 def do_sample(llm: vllm.LLM, lora_path: str, lora_id: int) -> list[str]:
-    question = (
-        "Mr. Sanchez found out that 40% of his Grade 5 students "
-        "got a final grade below B. How many of his students got "
-        "a final grade of B and above if he has 60 students in Grade 5?"
-    )
     prompts = [
-        PROMPT_TEMPLATE.format(question=question),
+        PROMPT_TEMPLATE.format(question="Count the number of farms."),
+        PROMPT_TEMPLATE.format(
+            question=(
+                "What is the average number of employees of the departments "
+                "whose rank is between 10 and 15?"
+            )
+        ),
+        PROMPT_TEMPLATE.format(
+            question=(
+                "What is the status of the city that has hosted the most competitions?"
+            )
+        ),
+        PROMPT_TEMPLATE.format(
+            question="which course has most number of registered students?"
+        ),
     ]
     sampling_params = vllm.SamplingParams(temperature=0, max_tokens=64)
     outputs = llm.generate(
@@ -48,11 +58,20 @@ def test_gptoss20b_lora(gptoss20b_lora_files):
     )
 
     expected_lora_output = [
-        "40% of 60 students = 0.4 x 60 = 24 students\n60 students - 24 students = 36 "
-        "students\nTherefore, 36 students got a final grade of B and above."
+        "SELECT COUNT(*) FROM farms",
+        "SELECT AVG(Num_Employees) FROM Department WHERE Rank BETWEEN 10 AND 15",
+        (
+            "SELECT status FROM city WHERE city_id IN (SELECT city_id FROM competition "
+            "GROUP BY city_id ORDER BY COUNT(*) DESC LIMIT 1);"
+        ),
+        (
+            "SELECT course_id, COUNT(*) AS num_of_students FROM register GROUP BY "
+            "course_id ORDER BY num_of_students DESC LIMIT 1;"
+        ),
     ]
 
     output1 = do_sample(llm, gptoss20b_lora_files, lora_id=1)
     print(output1)
     for i in range(len(expected_lora_output)):
-        assert output1[i].startswith(expected_lora_output[i])
+        cleaned_output = re.sub(r"\s+", " ", output1[i])
+        assert cleaned_output.startswith(expected_lora_output[i])
